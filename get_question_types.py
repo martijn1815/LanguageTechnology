5a7ca6d9290f45e368d6a9eca06ef66a896ba77e
Martijn Schendstok <sch.martijn@outlook.com>
2020-05-26 18:37:07 +0200

    Implemented the right format reading. Improved the code for writing files.

--- get_question_types.py
+++ get_question_types.py
@@ -10,46 +10,50 @@ from final_project import *
 
 def main(argv):
     nlp = spacy.load('en_core_web_sm')
-    open('where_question.txt', 'w').close()
-    open('who_question.txt', 'w').close()
-    open('x_of_y_question.txt', 'w').close()
-    open('true_false_question.txt', 'w').close()
-    open('description_question.txt', 'w').close()
-    open('other_question.txt', 'w').close()
 
-    with open("all_questions_sorted.txt", "r") as file:
+    where_file = open('questions\where_question.txt', 'w')
+    who_file = open('questions\who_question.txt', 'w')
+    xy_file = open('questions\\x_of_y_question.txt', 'w')
+    truefalse_file = open('questions\\true_false_question.txt', 'w')
+    description_file = open('questions\description_question.txt', 'w')
+    other_file = open('questions\other_question.txt', 'w')
+
+    with open("all_questions_formatted.txt", "r") as file:
         for line in file:
-            line = line.strip()
-            parse = nlp(line)
+            line = line.strip().split("\t")
+            n = line[0]
+            question = line[1]
+            parse = nlp(question)
 
             if is_where_question(parse):
                 # Where question:
-                with open("where_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                where_file.write("{0}\t{1}\n".format(n, question))
 
             elif is_xy_question(parse):
                 # X of Y question
-                with open("x_of_y_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                xy_file.write("{0}\t{1}\n".format(n, question))
 
             elif is_truefalse_question(parse):
                 # True/False question:
-                with open("true_false_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                truefalse_file.write("{0}\t{1}\n".format(n, question))
 
             elif is_description_question(parse):
                 # Description question:
-                with open("description_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                description_file.write("{0}\t{1}\n".format(n, question))
 
             elif is_who_question(parse):
                 # Who question:
-                with open("who_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                who_file.write("{0}\t{1}\n".format(n, question))
 
             else:
-                with open("other_question.txt", "a") as out_file:
-                    out_file.write(line + "\n")
+                other_file.write("{0}\t{1}\n".format(n, question))
+
+    where_file.close()
+    who_file.close()
+    xy_file.close()
+    truefalse_file.close()
+    description_file.close()
+    other_file.close()
 
 
 if __name__ == "__main__":
