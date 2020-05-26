793e1ac07efcd09434da75c28ac33025060699c0
Martijn Schendstok <sch.martijn@outlook.com>
2020-05-26 18:11:09 +0200

    Added program to get txt-files per question type based on the modular final_project.py

---
+++ get_question_types.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+File:       get_question_types.py
+Author:     Martijn E.N.F.L. Schendstok
+Date:       15 May 2020
+"""
+
+from final_project import *
+
+
+def main(argv):
+    nlp = spacy.load('en_core_web_sm')
+    open('where_question.txt', 'w').close()
+    open('who_question.txt', 'w').close()
+    open('x_of_y_question.txt', 'w').close()
+    open('true_false_question.txt', 'w').close()
+    open('description_question.txt', 'w').close()
+    open('other_question.txt', 'w').close()
+
+    with open("all_questions_sorted.txt", "r") as file:
+        for line in file:
+            line = line.strip()
+            parse = nlp(line)
+
+            if is_where_question(parse):
+                # Where question:
+                with open("where_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+            elif is_xy_question(parse):
+                # X of Y question
+                with open("x_of_y_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+            elif is_truefalse_question(parse):
+                # True/False question:
+                with open("true_false_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+            elif is_description_question(parse):
+                # Description question:
+                with open("description_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+            elif is_who_question(parse):
+                # Who question:
+                with open("who_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+            else:
+                with open("other_question.txt", "a") as out_file:
+                    out_file.write(line + "\n")
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
